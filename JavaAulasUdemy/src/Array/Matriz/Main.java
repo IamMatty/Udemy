fec3411f02afbe7b041284f4454653d4f961b08d
mateus.franca <mateus10gomes@gmail.com>
2020-04-21 00:02:34 -0300

    AulasUdemy 20/04/2020
    Vetores e matrizes

--- JavaAulasUdemy/src/Array/Matriz/Main.java
+++ JavaAulasUdemy/src/Array/Matriz/Main.java
@@ -1,10 +1,43 @@
 package Array.Matriz;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+			int matrizColumn = sc.nextInt();
+			int matrizRow = sc.nextInt();
+			int[][] matriz = new int[matrizColumn][matrizRow];
+			
+			for (int i = 0; i < matriz.length; i++) {
+				for (int j = 0; j < matriz[i].length; j++) {
+					matriz[i][j] = sc.nextInt();
+				}
+			}
+			System.out.println("Digite um número da matriz");
+			int matrizNumber = sc.nextInt();
+			
+			for (int i = 0; i < matriz.length; i++) {
+				for (int j = 0; j < matriz[i].length; j++) {
+					if (matrizNumber == matriz[i][j]) {
+						System.out.printf("Posição %d,%d:"+ i,j);
+						if (j > 0) {
+							System.out.printf("Esquerda: %d"+matriz[i][j-1]);							
+						}
+						if (i > 0) {
+							System.out.printf("Direita: %d"+matriz[i-1][j]);							
+						}
+						if (j > matriz[i].length-1) {
+							System.out.printf("Esquerda: %d"+matriz[i][j+1]);							
+						}
+						if (j > matriz.length-1) {
+							System.out.printf("Esquerda: %d"+matriz[i+1][j]);							
+						}
+					}
+				}
+			}
+		sc.close();
 	}
 
 }
